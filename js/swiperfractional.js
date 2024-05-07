3f1dcc179ff1f4c3147d4b4a6fec760b00276199
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-05-07 22:47:43 +0300

    adaptive swiper done

---
+++ js/swiperfractional.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiperReviews", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
