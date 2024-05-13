fe100d53d4cc79cfae297794019a8f65a084edd7
eugeniucc <eugeniucoloteniucc@gmail.com>
2024-05-13 11:06:58 +0300

    save

--- js/modal.js
+++ js/modal.js
@@ -1,18 +1,27 @@
 const popapWindow = document.querySelector(".popap");
-
 const popapOverlay = document.querySelector(".popap_overlay");
-
 const popapButton = document.querySelector(".popap_form-btn");
-
-// открыть по таймеру 15сек 1 раз
 
 function showModal() {
   popapWindow.classList.add("popap_show");
   popapOverlay.classList.add("popap_show");
 }
-setTimeout(showModal, 1000);
+
+let timerId = setTimeout(showModal, 1000);
 
 popapButton.addEventListener("click", () => {
   popapWindow.classList.remove("popap_show");
   popapOverlay.classList.remove("popap_show");
+  clearTimeout(timerId);
+});
+
+popapOverlay.addEventListener("keydown", function (event) {
+  if (
+    event.key === "Escape" &&
+    popapWindow.classList.contains("popap_show") &&
+    popapOverlay.classList.contains("popap_show")
+  ) {
+    popapWindow.classList.remove("popap_show");
+    popapOverlay.classList.remove("popap_show");
+  }
 });
